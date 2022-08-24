0904b6730b06a67525a777e1372b985a727a9816
nicolasnetz <nicolasnetz@github.com>
2022-08-24 09:11:12 -0400

    model with csv export

--- vrptw.py
+++ vrptw.py
@@ -1,15 +1,21 @@
+import pandas as pd
 import numpy as np
 from docplex.mp.model import Model
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 import numpy as np
 import sys
+import os
+import time
 
 class Instancia():
     def __init__(self,file_path, include_n_1 = False, num_nodos = None, ins_name="C101"):
         self.file_path = file_path
         file_head = []
         self.ins_name = ins_name
+        self.num_nodos = num_nodos
+        if num_nodos != None:
+            self.ins_name = "{}-{}".format(ins_name,str(num_nodos))
         cols_head = []
         instance_data = []
         with open(file_path) as f:
@@ -54,6 +60,7 @@ class Instancia():
 
         # Toth y Vigo
         # self.c = {(i,j): np.hypot(self.coord_x[i] - self.coord_x[j] , self.coord_y[i] - self.coord_y[j]) for i,j in self.A}
+        # KDMSS distance
         self.c = {(i,j): int(10*np.sqrt( (self.coord_x[i] - self.coord_x[j] )**2 + (self.coord_y[i] - self.coord_y[j])**2))/10 for i,j in self.A}
         # Solomon
         # self.c = {(i,j): np.round(np.hypot(self.coord_x[i] - self.coord_x[j] , self.coord_y[i] - self.coord_y[j]),1) for i in self.V for j in self.V if i!=j}
@@ -63,11 +70,13 @@ class Instancia():
         self.Q = self.vehicle_capacity
         self.q = list(self.demand.copy())
         self.M = {(i,j): np.max(self.b[i]+self.s[i]+self.c[i,j]-self.a[j],0) for i,j in self.A}
-
+        self.num_nodos = len(self.N)
 
 class Modelo():
-    def __init__(self):
+    def __init__(self,log = True, max_time = 30):
         self.name = "VRPTW1"
+        self.log = log
+        self.max_time = max_time
 
     def build(self,ins):
         # Model
@@ -98,17 +107,21 @@ class Modelo():
         self.mdl.add_constraints((self.mdl.sum(ins.q[i]*(self.mdl.sum(self.x[(i,j,k)] for j in ins.V if j not in [0,i])) for i in ins.N) <= ins.Q) for k in ins.K)
 
         # print(self.mdl.pprint_as_string())         
-        self.mdl.parameters.timelimit=3600
+        self.mdl.parameters.timelimit= self.max_time
         self.mdl.context.cplex_parameters.threads = 1
-        self.mdl.export_as_lp("model_nico.lp")
+        # self.mdl.export_as_lp("model_nico.lp")
 
 
     def solve(self):
-        solution = self.mdl.solve(log_output=True)
-
-        if solution:
+        self.start_time = time.time()
+        self.solution = self.mdl.solve(log_output=self.log)
+        self.finish_time = time.time()
+        self.elapsed_time = self.finish_time - self.start_time
+        self.result_type = str(self.mdl.solve_status).split(".")[1]
+        if self.solution:
             print("Obj: ",self.mdl.objective_value)
             self.mdl.report()
+            print("Result: ", self.result_type)
             
         else: 
             print("model has no solution...")
@@ -116,7 +129,7 @@ class Modelo():
     def export_sol(self,ins):
         print(self.mdl.solution)
 
-    def graficar_solucion(self,ins):
+    def graficar_solucion(self,ins,write_only = False):
         rutas = []
         truck = []
         for k in ins.K:
@@ -161,9 +174,12 @@ class Modelo():
 
         plt.xlabel("Distancia X")
         plt.ylabel("Distancia Y")
-        plt.title("Solución {} \n {}".format(ins.file_path,self.mdl.objective_value))
+        plt.title("Solución {} \n {}\n {}".format(ins.file_path,self.mdl.objective_value, self.result_type))
         plt.legend()
-        plt.show()
+        if write_only:
+            plt.savefig('my_plots/{}.png'.format(ins.ins_name))
+        else:    
+            plt.show()
 
     def export_route(self, ins):
         rutas = []
@@ -214,30 +230,80 @@ class Modelo():
                 formatted_routes.append(aux)
 
         with open('my_solutions/{}.sol'.format(ins.ins_name), 'w') as fp:
+            fp.write("{}\n".format(ins.ins_name))
             for i in range(1, len(formatted_routes) + 1):
                 fp.write("Route: #{}: {}\n".format(i, formatted_routes[i - 1]) )
                 
-            fp.write("{}\n".format(model.mdl.objective_value))
+            fp.write("{}\n".format(self.mdl.objective_value))
 
+    def export_to_table(self,ins):
+        if self.solution:
+            return [ins.ins_name, ins.num_nodos, "{0:.1f}".format(self.mdl.objective_value), self.result_type, "{0:.4g}".format(self.elapsed_time)]
+        else: 
+            return [ins.ins_name, ins.num_nodos, "DNF", self.result_type, "{0:.4g}".format(self.elapsed_time)]
 
+class InstanceSolver():
+    def __init__(self,ins_name = "C101", num_nodos= "10"):
+        self.ins_name = ins_name
+        if num_nodos == None:
+            self.num_nodos = num_nodos
+        else:
+            self.num_nodos = int(num_nodos)
+
+    def solve(self):
+        ins = Instancia("instances/{}.txt".format(self.ins_name),num_nodos=self.num_nodos, include_n_1=True, ins_name=self.ins_name) # FALSE FOR SOLOMON
+        model = Modelo(log=False, max_time=300)
+        print("building model")
+        model.build(ins)
+        print("model built")
+        model.solve()
+        if model.solution:
+            print("model solved")
+            model.graficar_solucion(ins, write_only = True)
+            model.export_route(ins)
+            
+        else:
+            print("model has no solution...")
+
+        print("export to table")
+        self.result =  model.export_to_table(ins)
+        print(self.result)
+
+    def get_result(self):
+        return(self.result)
 
 if __name__=="__main__":
-    # Ejemplo de entrada
-    # python main.py C101 10
-    ins_name = "C101"
-    num_nodos = 10
-    ins = Instancia("instances/{}.txt".format(ins_name),num_nodos=num_nodos, include_n_1=True, ins_name=ins_name) # FALSE FOR SOLOMON
-    model = Modelo()
-    print("building model")
-    model.build(ins)
-    # model.buildSolomon(ins)
-    # print(ins.V)
-    # print(ins.N)
-    print("model built")
-    model.solve()
-    model.graficar_solucion(ins)
-    model.export_route(ins)
+    try: 
+        os.mkdir("my_solutions")
+        os.mkdir("my_plots")
+    except:
+        pass
+    
+    
+    # ins_name = sys.argv[1]
+    # num_nodos = sys.argv[2]
+    # if num_nodos == "-1":
+    #     num_nodos = None
+    
+    results = []
+    for ins in os.listdir("instances"):
+        instance = InstanceSolver(ins_name=ins.split(".")[0], num_nodos= None)
+        # instance = InstanceSolver(ins_name=ins_name, num_nodos=num_nodos)
+        instance.solve()
+        results.append(instance.get_result())
+        df = pd.DataFrame(results, columns=["instance", "nodes", "result", "status", "time"])
+        df.to_csv("result_table.csv")
+    print(results)
+    
 
+# Tareas
+# Plot feasible
+# Export solutions when feasible
+# Except unknown and DNF
 
-# Mejorar el origen de los indices, para no generar el objeto iterador cada vez
- 
+# EJEMPLO DE USO
+# Especificando una cantidad de nodos = 5
+# root@user python vrptw.py R201 5
+
+# Si se quiere usar todos los nodos
+# root@user python vrptw.py R201 -1
